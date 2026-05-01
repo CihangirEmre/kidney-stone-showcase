36957553561790f32b00ce78a1a574df38f002ae
Cihangir Emre Er <CH6303@outlook.com>
2026-05-01 14:50:35 +0300

    import vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero'
 import LiveDemo from './components/LiveDemo'
 import Navbar from './components/Navbar'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Hero onDemoClick={() => document.getElementById('live-demo')?.scrollIntoView({ behavior: 'smooth' })} />
       <LiveDemo />
       <Navbar />
+      <Analytics />
     </>
   )
 }
